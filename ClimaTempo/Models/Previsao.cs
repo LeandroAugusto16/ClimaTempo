1ad16855f42761eeeb9dcdff8a10dc2158e3ed0b
LeandroAugusto16 <leandroaugustobap@gmail.com>
2024-10-18 22:40:01 -0300

    API

---
+++ ClimaTempo/Models/Previsao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimaTempo.Models
+{
+    internal class Previsao
+    {
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public DateTime Atualizado_em { get; set; }
+        public Clima clima { get; set; }
+    }
+}
